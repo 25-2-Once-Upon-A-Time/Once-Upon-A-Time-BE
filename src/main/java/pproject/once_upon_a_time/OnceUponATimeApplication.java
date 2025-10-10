89883785b7e1e917313b6121aca37f93a5a2137c
choes0101 <166037587+choes0101@users.noreply.github.com>
2025-10-10 10:08:16 +0900

    feat: Initial project structure

---
+++ src/main/java/pproject/once_upon_a_time/OnceUponATimeApplication.java
@@ -0,0 +1,13 @@
+package gachon.pproject.yenyet.once_upon_a_time;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnceUponATimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnceUponATimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gachon/pproject/yenyet/once_upon_a_time/OnceUponATimeApplicationTests.java
@@ -0,0 +1,13 @@
+package gachon.pproject.yenyet.once_upon_a_time;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnceUponATimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
